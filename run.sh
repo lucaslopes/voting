ddbfb1bb6b5e55b16591d2ea453a91c3db2e574d
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2024-11-30 00:53:28 -0300

    API auto update

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# Fetch the most current version of the repository
+echo "Fetching the latest version of the repository..."
+git pull
+echo "Repository updated successfully."
+
+# Check for and create virtual environment if necessary
 if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv -p 3.12 .venv
@@ -9,15 +15,16 @@ else
     echo "Virtual environment already exists."
 fi
 
+# Install dependencies
 uv pip install -U syftbox
 uv pip install -r requirements.txt
 
+# Activate the virtual environment
 . .venv/bin/activate
 
-# # run app using python from venv
+# Run the application
 echo "Running ring with $(python3 --version) at '$(which python3)'"
 python3 main.py
 
-# # deactivate the virtual environment
+# Deactivate the virtual environment
 deactivate
-
